4ce79542e477d7f7bb39d0ee20a7294752c996d8
Manas <manaskalamkar910@gmail.com>
2026-01-22 05:48:06 +0530

    feat: import shadcn UI components

--- src/components/blog/BlogCard.tsx
+++ src/components/blog/BlogCard.tsx
@@ -1,25 +1,31 @@
 import type { Blog } from '../../types/blog'
+import {
+  Card,
+  CardAction,
+  CardDescription, 
+  CardTitle,
+} from "@/components/ui/card"
 
 const BlogCard = ({ blog, setDetailed }: {
   blog: Blog, setDetailed: React.Dispatch<React.SetStateAction<string>>
 }) => {
 
   return (
-    <div className='flex flex-col gap-2.5 card border-2 border-gray-100 w-[90%] px-8 py-4 cursor-pointer' onClick={() => setDetailed(`${blog?.id}`)}>
+    <Card className='flex flex-col gap-2.5 card border-2 border-gray-100 w-[90%] px-8 py-4 cursor-pointer' onClick={() => setDetailed(`${blog?.id}`)}>
       <div className='typeandDate text-gray-400 flex justify-between'>
-        <div className='uppercase font-bold text-[0.9rem]'>{blog?.category[0]}</div>
+        <CardAction className='uppercase font-bold text-[0.9rem]'>{blog?.category[0]}</CardAction>
         <div className='lowercase text-[0.8rem]'>{Math.floor((Date.now() - Date.parse(blog?.date)) / (1000 * 60 * 60 * 24 * 7))} weeks ago</div>
       </div>
-      <div className='heading font-black text-2xl'>
+      <CardTitle className='heading font-black text-2xl'>
         {blog?.title}
-      </div>
-      <div className='description font-400 text-gray-600 text-[1.1rem]'>
+      </CardTitle>
+      <CardDescription >
         {blog?.description}
-      </div>
-      <div className="tags bg-gray-300 w-fit px-2 py-1 rounded-full text-[0.9rem]">
-        {blog?.category[1]??blog?.category[0]}
-      </div>
-    </div>
+      </CardDescription>
+      <CardAction className="tags bg-gray-300 w-fit px-4 py-1 rounded-full text-[0.9rem]">
+        {blog?.category[1] ?? blog?.category[0]}
+      </CardAction>
+    </Card>
   )
 }
 
--- src/components/blog/BlogDetail.tsx
+++ src/components/blog/BlogDetail.tsx
@@ -1,4 +1,21 @@
 import { useBlog } from '../../hooks/useBlogs'
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { BlogDetailSkeleton } from '../layout/Loading';
 
 const BlogDetail = ({ detailed }: {
   detailed: string
@@ -7,43 +24,61 @@ const BlogDetail = ({ detailed }: {
 
   const { data: blog, error, isPending, isFetching } = useBlog(detailed);
 
-  if (isPending) return "Loading";
+  if (isPending) return <BlogDetailSkeleton />;
 
   if (error) return 'An error has occurred: ' + error?.message;
 
   return (
-    <div className='w-[72%] flex flex-col items-center mx-10 shadow-2xl border-shadow-2xl'>
+    <Card className='py-0 h-fit w-[72%] flex flex-col items-center mx-10 shadow-2xl '>
       <img src={blog?.coverImage} alt={"cover image"} className='w-full h-200 object-cover rounded-t-2xl' />
-      <div className="data flex flex-col text-left mx-15 my-15 gap-8">
-        <div className="typeAndDate flex gap-4">
+
+      <div className="data flex flex-col text-left mx-15 my-15 gap-16">
+        <CardAction className="typeAndDate flex gap-4">
           <div className='uppercase font-bold text-violet-600'>{blog?.category.join(" • ")}</div>•
-          <div className='lowercase'>{parseInt((blog?.date!).split("T")[1].split(":")[0]) <= 7 ? 5:10} min read</div>
-        </div>
-        <div className="heading text-6xl font-extrabold">
-          {blog?.title}
-        </div>
-        <button className='bg-violet-600 w-fit px-4 py-2 rounded-md text-gray-100 font-bold'>Share Article</button>
-        <div className="details py-1 flex justify-around border-gray-50 rounded-md bg-gray-200 text-center">
-          <div className='w-[33%] py-10 bg-gray-100'>
-            <div className="category uppercase font-bold text-gray-500">Category</div>
-            <div className="subheading font-bold text-xl ">{blog?.category.join(" & ")} </div>
-          </div>
-          <div className='w-[33%] py-10 bg-gray-100'>
-            <div className="readTime uppercase font-bold text-gray-500">Read Time</div>
-            <div className="subheading font-bold text-xl ">{parseInt((blog?.date!).split("T")[1].split(":")[0]) <= 7 ? 5:10} Mins</div>
-          </div>
-          <div className='w-[33%] py-10 bg-gray-100'>
-            <div className="date uppercase font-bold text-gray-500">Date</div>
-            <div className="subheading font-bold text-xl ">{(blog?.date!).split("T")[0]}</div>
-          </div>
-        </div>
-        <div className="content text-2xl">
+          <div className='lowercase'>{parseInt((blog?.date!).split("T")[1].split(":")[0]) <= 7 ? 5 : 10} min read</div>
+        </CardAction>
+
+        <CardHeader className='flex flex-col gap-6'>
+          <CardContent className='flex flex-row w-full justify-between'>
+            <CardTitle className="heading text-6xl font-extrabold">
+              {blog?.title}
+            </CardTitle>
+            <CardAction>
+              <Button className='cursor-pointer w-fit h-fit  text-gray-100 font-bold' >Share Article</Button>
+            </CardAction>
+          </CardContent>
+
+
+
+          <Table>
+            <TableHeader >
+              <TableRow>
+                <TableHead className="category uppercase font-bold text-gray-500">Category</TableHead>
+                <TableHead className="readTime uppercase font-bold text-gray-500">Read Time</TableHead>
+                <TableHead className="date uppercase font-bold text-gray-500">Date</TableHead>
+              </TableRow>
+
+            </TableHeader>
+            <TableBody>
+
+              <TableRow>
+                <TableCell className="subheading font-bold text-xl ">{blog?.category.join(" & ")} </TableCell>
+                <TableCell className="subheading font-bold text-xl ">{parseInt((blog?.date!).split("T")[1].split(":")[0]) <= 7 ? 5 : 10} Mins</TableCell>
+                <TableCell className="subheading font-bold text-xl ">{(blog?.date!).split("T")[0]}</TableCell>
+
+              </TableRow>
+            </TableBody>
+
+
+          </Table>
+        </CardHeader>
+        <CardContent className="content text-2xl">
           {blog?.content}
-        </div>
+        </CardContent>
         <div className="tags"></div>
-        {isFetching ? "Updating...":""}
+        {isFetching ? "Updating..." : ""}
       </div>
-    </div>
+    </Card >
   )
 }
 
