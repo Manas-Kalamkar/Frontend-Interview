a45ae918f8a415c8caf1c68e9649352d34111aa4
Manas <manaskalamkar910@gmail.com>
2026-01-31 03:46:50 +0530

    feat: data fetch from supabase comment get,post method

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,35 @@
 // api.ts
 
 import type { postBlogInterface } from "../types/blog";
-
+import supabase from "@/supabaseClient";
 
 export const postBlog = async (Blog: postBlogInterface) => {
 console.log("POSTING:", Blog);
 
-  const res = await fetch("http://localhost:3001/blogs", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(Blog),
-  });
-  if (!res.ok){
-    throw new Error("Failed to Create blog")
+  const {data,error} = await supabase
+  .from("blogs")
+  .insert([Blog])
+  .select()
+  .single();
+
+  if(error){
+    console.error("Supabase Error: ",error.message);
+    throw new Error(error.message);
   }
 
-  return res.json();
+  //Following is the code for the fetch the post method for active server
+  // const res = await   fetch("http://localhost:3001/blogs", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   body: JSON.stringify(Blog),
+  // });
+  // if (!res.ok){
+  //   throw new Error("Failed to Create blog")
+  // }
+
+  // return res.json();
+
+  return data;
 };
--- src/hooks/useBlogs.ts
+++ src/hooks/useBlogs.ts
@@ -1,35 +1,58 @@
 import { useQuery } from "@tanstack/react-query";
 import type { Blog } from "../types/blog";
+import supabase from "@/supabaseClient";
 
 export const useBlogs = () => {
-    const { data, isPending, error, isFetching } = useQuery<Blog[], Error>({
+  const { data, isPending, error, isFetching } = useQuery<Blog[], Error>({
     queryKey: ["blogsData"],
     queryFn: async () => {
-      const response = await fetch(
-        'http://localhost:3001/blogs'
-      );
+      const { data, error } = await supabase
+        .from("blogs")
+        .select("*")
+        .order("date", { ascending: false })
+      // const response = await fetch(
+      //   'http://localhost:3001/blogs'
+      // );
 
-      if (!response.ok) {
-        throw new Error(`Server error: ${response.status}`)
-      }
-      return await response.json();
+      // if (!response.ok) {
+      //   throw new Error(`Server error: ${response.status}`)
+      // }
+      // return await response.json();
+      if (error) throw new Error(error.message);
+      return data as Blog[];
     },
   });
 
   return { data, isPending, error, isFetching }
 }
-export const useBlog = (id:string) => {
-    const { data, isPending, error, isFetching } = useQuery<Blog, Error>({
+export const useBlog = (id: string) => {
+  const { data, isPending, error, isFetching } = useQuery<Blog, Error>({
     queryKey: ["blog", id],
     queryFn: async () => {
-      const rep = await fetch(
-        `http://localhost:3001/blogs/${id}`
-      );
-      if (!rep.ok) {
-        console.log(rep.status)
-        throw new Error(`Server error: ${rep.status}`)
-      }
-      return await rep.json();
+
+
+      const { data, error } = await supabase
+        .from("blogs")
+        .select("*")
+        .eq("id", id)
+        .single();
+
+      if (error) throw new Error(error.message);
+      if (!data) throw new Error("Blog not found")
+
+      return data as Blog;
+
+      //   const rep = await fetch(
+      //     `http://localhost:3001/blogs/${id}`
+      //   );
+      //   if (!rep.ok) {
+      //     console.log(rep.status)
+      //     throw new Error(`Server error: ${rep.status}`)
+      //   }
+      //   return await rep.json();
+
+
+
     }
   });
   return { data, isPending, error, isFetching }
